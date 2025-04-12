8c3fc28365fb341787b10f02010bc47d47b61fc0
josers77j <josers77.1@gmail.com>
2025-04-11 23:36:17 -0600

    feat: validators for register page

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,39 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators, FormGroup, AbstractControl } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-register-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './register-page.component.html',
 })
-export class RegisterPageComponent { }
+export class RegisterPageComponent {
+
+  formBuilder = inject(FormBuilder);
+
+  formUtils = FormUtils;
+
+  myForm = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.pattern(this.formUtils.namePattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.formUtils.emailPattern)], [this.formUtils.checkingServerResponse]],
+    username: ['', [Validators.required, Validators.minLength(6), Validators.pattern(this.formUtils.notOnlySpacesPattern), this.formUtils.noStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required]],
+  },
+    {
+      validators: [
+        this.formUtils.isFieldOneEqualFieldTwo('password', 'confirmPassword'),
+      ]
+    }
+
+  );
+
+
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
+
+
+}
--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,29 +1,53 @@
-import { FormArray, FormGroup, ValidationErrors } from "@angular/forms";
+import { AbstractControl, FormArray, FormGroup, ValidationErrors } from "@angular/forms";
 
+async function sleep() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(true);
+    }, 2000);
+  })
+}
 export class FormUtils {
 
-  static getTextError(errors: ValidationErrors){
-    for(const key of Object.keys(errors)) {
-      switch(key){
+  static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
+
+  static getTextError(errors: ValidationErrors) {
+    for (const key of Object.keys(errors)) {
+      switch (key) {
         case 'required':
           return 'Este campo es requerido';
         case 'minlength':
           return `Este campo debe tener al menos ${errors['minlength'].requiredLength} caracteres`;
         case 'min':
           return `Este campo debe ser mayor a ${errors['min'].min}`;
+        case 'email':
+          return 'El formato del email es incorrecto';
+        case 'pattern':
+          if (errors['pattern'].requiredPattern === FormUtils.emailPattern) {
+            return 'El formato del email es incorrecto';
+          }
+          return 'Error de patron contra expresion regular';
+        case 'passwordNotEqual':
+          return 'Las contraseñas no coinciden';
+        case 'emailTaken':
+          return 'El email ya fue tomado';
+        case 'noStrider':
+          return 'El nombre strider no es permitido';
+        default:
+          return 'Error de validacion no controlado'
       }
     }
     return null;
   }
 
-  static isValidField(form: FormGroup ,fieldName: string): boolean | null {
-    console.log('isValidField', fieldName, form.controls[fieldName].errors);
-    console.log('isValidField', !!form.controls[fieldName].errors && form.controls[fieldName].touched);
+  static isValidField(form: FormGroup, fieldName: string): boolean | null {
     return !!form.controls[fieldName].errors && form.controls[fieldName].touched
   }
 
-  static getFieldError(form: FormGroup,fieldName: string): string | null {
-    if(!form.controls[fieldName])
+  static getFieldError(form: FormGroup, fieldName: string): string | null {
+    if (!form.controls[fieldName])
       return null;
 
     const errors = form.controls[fieldName].errors ?? {};
@@ -32,16 +56,45 @@ export class FormUtils {
 
   }
 
-  static isValidFieldInArray(formArray: FormArray, index:number){
+  static isValidFieldInArray(formArray: FormArray, index: number) {
     return (formArray.controls[index].errors && formArray.controls[index].touched)
   }
 
-  static getFieldErrorInArray(formArray: FormArray, index:number): string | null {
-    if(formArray.controls.length === 0) return null;
+  static getFieldErrorInArray(formArray: FormArray, index: number): string | null {
+    if (formArray.controls.length === 0) return null;
 
     const errors = formArray.controls[index].errors ?? {};
 
     return this.getTextError(errors);
+
+  }
+
+  static isFieldOneEqualFieldTwo(fieldOne: string, fieldTwo: string) {
+    return (FormGroup: AbstractControl) => {
+      const fieldOneValue = FormGroup.get(fieldOne)?.value;
+      const fieldTwoValue = FormGroup.get(fieldTwo)?.value;
+
+      return fieldOneValue === fieldTwoValue ? null : { passwordNotEqual: true };
+    }
+  }
+
+  static async checkingServerResponse(control: AbstractControl): Promise<ValidationErrors | null> {
+    await sleep();
+
+    const formValue = control.value;
+
+    if (formValue === 'hola@mundo.com') {
+      return { emailTaken: true };
+    }
+    return null;
+  }
+
+  static noStrider(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+    if (value === 'strider') {
+      return { noStrider: true };
+    }
+    return null;
 
   }
 }
